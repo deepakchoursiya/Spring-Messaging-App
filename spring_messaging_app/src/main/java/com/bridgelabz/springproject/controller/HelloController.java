6e539c3b8c61a3bcd7429054a0c9603b9309046e
deepakchoursiya <chourasiadeepak2002@gmail.com>
2025-02-21 23:12:04 +0530

    Deepak Add. UC2

--- spring_messaging_app/src/main/java/com/bridgelabz/springproject/controller/HelloController.java
+++ spring_messaging_app/src/main/java/com/bridgelabz/springproject/controller/HelloController.java
@@ -1,15 +1,23 @@
 package com.bridgelabz.springproject.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/hello")
+@RequestMapping("/helo")
 public class HelloController {
 
+    // UC1: Default message
     @GetMapping
     public String sayHello() {
         return "Hello from BridgeLabz";
+    }
+
+    // UC2: Accepting name as a query parameter
+    @GetMapping("/query")
+    public String sayHelloWithName(@RequestParam String name) {
+        return "Hello " + name + " from BridgeLabz";
 }
 }
