52633ee9857ad1fd13b8c580957a82e9eed2c2b6
deepakchoursiya <chourasiadeepak2002@gmail.com>
2025-02-21 23:35:56 +0530

    Deepak Add. UC4

--- spring_messaging_app/src/main/java/com/bridgelabz/springproject/controller/HelloController.java
+++ spring_messaging_app/src/main/java/com/bridgelabz/springproject/controller/HelloController.java
@@ -1,10 +1,11 @@
 package com.bridgelabz.springproject.controller;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
+import com.bridgelabz.springproject.dto.*;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/hello")
@@ -26,5 +27,11 @@ public class HelloController {
     @GetMapping("/param/{name}")
     public String sayHelloWithPathVariable(@PathVariable String name) {
         return "Hello " + name + " from BridgeLabz";
+    }
+
+    // UC4: Accepting firstName and lastName from request body
+    @PostMapping("/post")
+    public String sayHelloWithPost(@RequestBody User user) {
+        return "Hello " + user.getFirstName() + " " + user.getLastName() + " from BridgeLabz";
 }
 }
